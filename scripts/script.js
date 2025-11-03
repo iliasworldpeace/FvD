4f3c6f8d70273efc9a653d0104288d03bbb5858e
iliasworldpeace <84684421+iliasworldpeace@users.noreply.github.com>
2025-11-03 01:22:36 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,26 @@
-const lang = document.querySelector("nav>li:nth-of-type(1)>ul>li");
-const button = document.querySelector("nav>li:nth-of-type(1)");
+const lang = document.querySelector("nav>ul:nth-of-type(1)>li:nth-of-type(1)>ul");
+const button = document.querySelector("nav>:nth-of-type(1)>li:nth-of-type(1) button");
 
 // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
  
-button.addEventListener("click", openMenu);
+button.addEventListener("click", langOpen);
  
 // stap 3: voeg in de functie een class toe aan de nav
-function openMenu() {
-    lang.classList.toggle("menuOpen");
+function langOpen() {
+    lang.classList.toggle("langOpen");
     console.log("hoi")
 }
+ 
+const prive = document.querySelector("nav>ul:nth-of-type(1)>li:nth-of-type(2)>ul");
+const priveButton = document.querySelector("nav>:nth-of-type(1)>li:nth-of-type(2) button");
+
+ 
+priveButton.addEventListener("click", priveOpen);
+
+function priveOpen() {
+    prive.classList.toggle("priveOpen");
+    console.log("hoi")
+}
+
+/* met dank aan Mats Groot*/
  
